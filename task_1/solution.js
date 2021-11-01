c9653bc4053f341171d4ed931145aed9e0490b0a
zvasileva <zojka-homka@rambler.ru>
2021-11-01 19:11:43 +0300

    Решение task_1

--- task_1/solution.js
+++ task_1/solution.js
@@ -14,6 +14,17 @@ function addInCartMessage(title, price) {
     // добавьте в конец переменной message текст 'теперь в корзине!'
     // выведите переменную message в консоль
 
+    let message = productName;
+
+    message = `${message} `;
+    message = `${message}за`;
+    message = `${message} `;
+    message = `${message}${productPrice}`;
+    message = `${message} `;
+    message = `${message}теперь в корзине!`;
+
+    console.log(message);
+
     // Конец решения задания №1.1.
 
     return message;
@@ -27,6 +38,9 @@ function addInCartCountChange(value) {
     // создайте переменную newValue
     // присвойте ей значение oldValue
     // увеличьте значение newValue на 1
+    let newValue = oldValue; 
+    
+    newValue = newValue + 1;
 
     // Конец решения задания №1.2.
 
@@ -42,6 +56,9 @@ function addInCartSumChange(sum, delta) {
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
+
+    let newSum = +(oldSum)  + +(difference);
+    let newSumText = `${newSum} ₽`;
 
     // Конец решения задания №1.3.
 
